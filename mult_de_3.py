80fb4f45150300106d61c13a32098237350dec23
Vinícius-Freire <120794204+vinicius-freire-oliveira@users.noreply.github.com>
2024-05-29 16:41:12 -0300

    Add files via upload
    A partir das variáveis tem-se os múltiplos de 3

---
+++ mult_de_3.py
@@ -0,0 +1,10 @@
+# Define o último número a ser impresso
+fim = 30
+
+# Inicializa a variável x como 3
+x = 3
+
+# Loop para imprimir os múltiplos de 3 até o número 30
+while x <= fim:
+    print(x)
+    x = x + 3
